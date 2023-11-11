d85468105024924f887cd9c6f0f2a0c061692c08
JoMds <Giannis58@Hotmail.com>
2023-11-11 19:26:47 +0200

    temp

--- src/main/java/com/example/dataProcessing/KafkaListeners.java
+++ src/main/java/com/example/dataProcessing/KafkaListeners.java
@@ -3,11 +3,25 @@ package com.example.dataProcessing;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.example.dataProcessing.model.ProcessedData;
+import com.example.dataProcessing.repository.DataRepository;
+
 @Component
 public class KafkaListeners {
+
+    private DataRepository repository;
     
+    public KafkaListeners(DataRepository repository) {
+        this.repository = repository;
+    }
+
     @KafkaListener(topics = "proccesedData", groupId = "proccesed_Data")
     void listener(String data){
+
+        ProcessedData obj = new ProcessedData();
+        obj.setMessage(data);
+
+        repository.save(obj);
 
         System.out.println(data);
 
